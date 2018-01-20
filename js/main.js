7b3e7f272220647445d44a0d9a75425e24eeed0a
Azam <azam00399@gmail.com>
2018-01-20 22:50:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -28,4 +28,14 @@ $(document).ready(function(){
             }
         });
     }
+
+    $(".slowly").on("click", function(event){
+        event.preventDefault();
+
+        var id = $(this).attr('href');
+
+        var top = $(id).offset().top;
+
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
